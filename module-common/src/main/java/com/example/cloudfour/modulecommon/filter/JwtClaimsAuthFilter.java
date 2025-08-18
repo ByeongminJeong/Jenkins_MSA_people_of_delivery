d2065d027d07a36850fb8b0e7785f82876864b6c
jiyoonkim <rlawldbs1007@gmail.com>
2025-08-19 07:56:39 +0900

    🔨refactor : Spring SecurityConfig 수정 - "**/actuator/**" 추가

--- module-common/src/main/java/com/example/cloudfour/modulecommon/filter/JwtClaimsAuthFilter.java
+++ module-common/src/main/java/com/example/cloudfour/modulecommon/filter/JwtClaimsAuthFilter.java
@@ -26,15 +26,6 @@ public class JwtClaimsAuthFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
             throws ServletException, IOException {
 
-//        // 헬스체크 요청은 그냥 통과하도록
-//        String path = request.getRequestURI();
-//        if (path != null && path.contains("/actuator/health")) {
-//            // 아예 강제로 OK 리턴
-//            response.setStatus(HttpServletResponse.SC_OK);
-//            response.getWriter().write("{\"status\":\"UP\"}");
-//            return;
-//        }
-
         var auth = request.getHeader(HttpHeaders.AUTHORIZATION);
         if (StringUtils.hasText(auth) && auth.startsWith("Bearer ")) {
             String token = auth.substring(7);
