ed406bd5bac71caf20357ac25aa1fa2dbaf03827
jiyoonkim <rlawldbs1007@gmail.com>
2025-08-18 16:05:43 +0900

    🔨refactor : spring config 설정 제거 - import org.springframework.cloud.client.discovery.EnableDiscoveryClient; 주석처리

--- cart-service/src/main/java/com/example/cloudfour/cartservice/CartServiceApplication.java
+++ cart-service/src/main/java/com/example/cloudfour/cartservice/CartServiceApplication.java
@@ -2,7 +2,7 @@ package com.example.cloudfour.cartservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
