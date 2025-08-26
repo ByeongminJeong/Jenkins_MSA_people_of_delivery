c2bcab940246e8ae8835c0d7435c155dc24f1dfa
jiyoonkim <rlawldbs1007@gmail.com>
2025-08-26 23:24:14 +0900

    user test

--- auth-service/src/main/java/com/example/cloudfour/authservice/client/UserClient.java
+++ auth-service/src/main/java/com/example/cloudfour/authservice/client/UserClient.java
@@ -17,7 +17,7 @@ public class UserClient {
 
     //private static final String BASE = "http://user-service/internal/users";
     //private static final String BASE = "http://user-service.peopleofdelivery:8014/internal/users";
-    private static final String BASE = "http://internal-alb-internal-902295756.ap-northeast-2.elb.amazonaws.com/internal/users";
+    private static final String BASE = "http://internal-alb-internal-902295756.ap-northeast-2.elb.amazonaws.com/users";
 
     public UserResponseDTO.ExistsByEmailResponseDTO existsByEmail(String email) {
         Map<String, Object> params = new HashMap<>();
