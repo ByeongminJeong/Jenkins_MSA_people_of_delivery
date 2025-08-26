e3cd3b4ab02da7178aac311082cabc09ba9e12e3
jiyoonkim <rlawldbs1007@gmail.com>
2025-08-26 17:47:23 +0900

    cart 수정 - @LoadBalanced 삭제

--- cart-service/src/main/java/com/example/cloudfour/cartservice/config/RestTemplateConfig.java
+++ cart-service/src/main/java/com/example/cloudfour/cartservice/config/RestTemplateConfig.java
@@ -2,7 +2,7 @@ package com.example.cloudfour.cartservice.config;
 
 import com.example.cloudfour.modulecommon.error.RestTemplateResponseErrorHandler;
 import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+//import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -11,7 +11,7 @@ import org.springframework.web.client.RestTemplate;
 public class RestTemplateConfig {
 
     @Bean
-    @LoadBalanced
+    //@LoadBalanced
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
         return builder.errorHandler(new RestTemplateResponseErrorHandler()).build();
     }
