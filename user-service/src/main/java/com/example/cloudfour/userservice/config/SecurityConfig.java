5e5f8d801a08ff1683097ddfd89c66b52d996b56
jiyoonkim <rlawldbs1007@gmail.com>
2025-08-27 13:24:21 +0900

    USER CONFIG 경로 추가

--- user-service/src/main/java/com/example/cloudfour/userservice/config/SecurityConfig.java
+++ user-service/src/main/java/com/example/cloudfour/userservice/config/SecurityConfig.java
@@ -28,7 +28,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/internal/**", "/actuator/**", "/v3/api-docs/**", "/swagger-ui/**").permitAll()
+                        .requestMatchers("/internal/**", "/actuator/**", "/v3/api-docs/**", "/swagger-ui/**", "/users/actuator/**", "/store/actuator/**", "/module-common/actuator/**", "/carts/actuator/**", "/auth/actuator/**", "/payments/actuator/**", "/ai/actuator/**").permitAll()
                         .requestMatchers("/users/**", "/profile/**").authenticated()
                         .anyRequest().authenticated()
                 )
